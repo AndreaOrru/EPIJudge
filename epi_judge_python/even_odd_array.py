2a5b8632df720f4fc84e4d5607313173054d6b15
Andrea Orru <andrea@orru.io>
2020-03-28 18:47:52 +0900

    Even odd array

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -7,9 +7,35 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+def even_odd_storage(A: List[int]) -> None:
+    result = []
+
+    for el in A:
+        if el % 2 == 0:
+            result.append(el)
+    for el in A:
+        if el % 2 == 1:
+            result.append(el)
+    for i, el in enumerate(result):
+        A[i] = el
+
+
 def even_odd(A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    next_even = 0
+    next_odd = len(A) - 1
+
+    while True:
+        while next_even < next_odd and A[next_even] % 2 == 0:
+            next_even += 1
+        while next_even < next_odd and A[next_odd] % 2 == 1:
+            next_odd -= 1
+
+        if next_even >= next_odd:
+            break
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_even += 1
+            next_odd -= 1
 
 
 @enable_executor_hook
