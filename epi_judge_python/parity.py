cf981bfcb8d0951ef134a96c274cb88a086c225c
Andrea Orru <andrea@orru.io>
2020-03-28 18:08:13 +0900

    Parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,57 @@
 from test_framework import generic_test
 
 
+def parity_brute_force(x: int) -> int:
+    result = 0
+
+    while x:
+        result ^= x & 1
+        x >>= 1
+
+    return result
+
+
+def parity_and_trick(x: int) -> int:
+    result = 0
+
+    while x:
+        result ^= 1
+        # Drop lowest set bit of x.
+        x &= x - 1
+
+    return result
+
+
+parity_table = {}
+
+def compute_parity_table():
+    global parity_table
+    if parity_table:
+        return
+
+    for i in range(2 ** 16):
+        parity_table[i] = parity_and_trick(i)
+
+
+def parity_with_table(x: int) -> int:
+    compute_parity_table()
+    result = 0
+
+    for _ in range(64 // 16):
+        result ^= parity_table[x & 0xFFFF]
+        x >>= 16
+
+    return result
+
+
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 1
 
 
 if __name__ == '__main__':
