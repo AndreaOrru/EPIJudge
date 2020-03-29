096e3c89a52b3a628d5f2c0d65d5685395841228
Andrea Orru <andrea@orru.io>
2020-03-29 10:51:07 +0900

    String integer interconversion

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -2,14 +2,41 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
+def num_to_char(digit: int) -> str:
+    return chr(ord('0') + digit)
+
+
+def char_to_num(char: str) -> int:
+    return ord(char) - ord('0')
+
+
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    result = []
+    negative = x < 0
+    x = -x if negative else x
+
+    while True:
+        result.append(num_to_char(x % 10))
+        x //= 10
+        if x == 0:
+            break
+
+    if negative:
+        result.append('-')
+
+    return ''.join(reversed(result))
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+
+    for power, ch in enumerate(reversed(s)):
+        if ch in ('+', '-'):
+            break
+        digit = char_to_num(ch)
+        result += digit * (10 ** power)
+
+    return -result if s[0] == '-' else result
 
 
 def wrapper(x, s):
