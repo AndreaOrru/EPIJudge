4491c2e6e5984326a5676f52ae53b510ea4d7977
Andrea Orru <andrea@orru.io>
2020-03-29 10:04:26 +0900

    Spiral ordering, offline sampling

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -3,9 +3,49 @@ from typing import List
 from test_framework import generic_test
 
 
+def spiral_order_rec(M, y, x, size, is_top_left):
+    if size == 0:
+        return []
+    
+    result = []
+    mul = 1 if is_top_left else -1
+
+    for i in range(size):
+        result.append(M[y][x + i*mul])
+    for j in range(1, size):
+        result.append(M[y + j*mul][x + i*mul])
+
+    y += (size - 1) * mul
+    x += (size - 2) * mul
+
+    return result + spiral_order_rec(M, y, x, size - 1, not is_top_left)
+
+
+def spiral_order(M):
+    result = []
+
+    size = len(M)
+    y = x = 0
+    mul = 1
+
+    while size:
+        for i in range(size):
+            result.append(M[y][x + i*mul])
+        for j in range(1, size):
+            result.append(M[y + j*mul][x + i*mul])
+
+        y += (size - 1) * mul
+        x += (size - 2) * mul
+
+        mul = -mul
+        size -= 1
+
+    return result
+
+
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return spiral_order(square_matrix)
+    #return spiral_order(square_matrix, 0, 0, len(square_matrix), True)
 
 
 if __name__ == '__main__':
