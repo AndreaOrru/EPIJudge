d58ade585189538a8e09c73805e4ce9572cdd096
Andrea Orru <andrea@orru.io>
2020-03-29 16:17:13 +0900

    Reverse sublist

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -6,8 +6,21 @@ from test_framework import generic_test
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = sublist_head = ListNode(None, L)
+
+    for _ in range(1, start):
+        sublist_head = sublist_head.next
+    
+    sublist_iter = sublist_head.next
+    for _ in range(finish - start):
+        rest = sublist_iter.next.next
+        element_to_move = sublist_iter.next
+
+        sublist_iter.next = rest
+        element_to_move.next = sublist_head.next
+        sublist_head.next = element_to_move
+
+    return dummy_head.next
 
 
 if __name__ == '__main__':
