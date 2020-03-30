cc50d1b7e5b0a46b3411d2b1151672566da89d8d
Andrea Orru <andrea@orru.io>
2020-03-30 14:15:26 +0900

    Int square root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
 
+def square_root_brute_force(k: int) -> int:
+    n = 0
+    while (n * n) <= k:
+        n += 1
+    return n if (n * n) == k else n - 1
+
+
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    lower, upper = 0, k
+
+    while lower <= upper:
+        mid = (lower + upper) // 2
+        mid_squared = mid * mid
+
+        if k < mid_squared:
+            upper = mid - 1
+        elif k > mid_squared:
+            lower = mid + 1
+        else:
+            return mid
+
+    return lower - 1
 
 
 if __name__ == '__main__':
