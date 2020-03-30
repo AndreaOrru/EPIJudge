b2c255ba9dc4beaadf03ea822ba186a1b98d2bd7
Andrea Orru <andrea@orru.io>
2020-03-30 13:05:33 +0900

    Merge arrays

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -2,10 +2,29 @@ from typing import List
 
 from test_framework import generic_test
 
+from heapq import heappush, heappop
+
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    min_heap = []
+    array_iters = [iter(a) for a in sorted_arrays]
+
+    for i, it in enumerate(array_iters):
+        first_elem = next(it, None)
+        if first_elem is not None:
+            heappush(min_heap, (first_elem, i))
+
+    while min_heap:
+        smallest_elem, smallest_array_i = heappop(min_heap)
+        smallest_array_iter = array_iters[smallest_array_i]
+        result.append(smallest_elem)
+
+        next_elem = next(smallest_array_iter, None)
+        if next_elem is not None:
+            heappush(min_heap, (next_elem, smallest_array_i))
+
+    return result
 
 
 if __name__ == '__main__':
