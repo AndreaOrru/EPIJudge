54671fa387537d9561c725bca61f896e382116e9
Andrea Orru <andrea@orru.io>
2020-03-31 16:12:32 +0900

    n_queens

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,8 +4,28 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def attacked(row, col):
+        for y in range(row):
+            if col_placements[y] == col:
+                return True
+            if abs(col_placements[y] - col) == abs(y - row):
+                return True
+        return False
+
+    def solve_queens(row):
+        if row == n:
+            results.append(col_placements.copy())
+            return
+        for col in range(n):
+            if attacked(row, col):
+                continue
+            col_placements[row] = col
+            solve_queens(row + 1)
+
+    results = []
+    col_placements = [0] * n
+    solve_queens(0)
+    return results
 
 
 def comp(a, b):
