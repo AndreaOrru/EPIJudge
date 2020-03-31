a03f7f881dd5c8d32a235f5292a810a22e89212f
Andrea Orru <andrea@orru.io>
2020-03-31 16:35:50 +0900

    Fibonacci

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -3,10 +3,21 @@ from functools import lru_cache
 
 
 @lru_cache(None)
+def fibonacci_lru(n: int) -> int:
+    if n <= 1:
+        return n
+    return fibonacci_lru(n - 1) + fibonacci_lru(n - 2)
+
+
 def fibonacci(n: int) -> int:
     if n <= 1:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+    a, b = 0, 1
+    for _ in range(n - 1):
+        a, b = b, a + b
+
+    return b
 
 
 if __name__ == '__main__':
