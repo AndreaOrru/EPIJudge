1ff397e28cc1a15e3b7a969f85955cb1a801c35e
Andrea Orru <andrea@orru.io>
2020-03-31 14:02:01 +0900

    Is tree a BST

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,9 +2,20 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+def is_binary_tree_bst_helper(tree, lower_bound=float('-inf'), upper_bound=float('inf')):
+    if tree is None:
+        return True
+    if tree.data < lower_bound:
+        return False
+    if tree.data > upper_bound:
+        return False
+
+    return is_binary_tree_bst_helper(tree.left, lower_bound, tree.data) and \
+           is_binary_tree_bst_helper(tree.right, tree.data, upper_bound)
+
+
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    return is_binary_tree_bst_helper(tree)
 
 
 if __name__ == '__main__':
