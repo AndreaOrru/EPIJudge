4afd979d6ff201fe58a3b69d7e9707af6074820c
Andrea Orru <andrea@orru.io>
2020-04-01 15:31:10 +0900

    Maze

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -14,8 +14,37 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
-    # TODO - you fill in here.
-    return []
+    def dfs_search(c: Coordinate) -> bool:
+        # Exclude invalid coordinates.
+        if not (0 <= c.x < len(maze) and
+                0 <= c.y < len(maze[0]) and
+                maze[c.x][c.y] == WHITE):
+            return False
+
+        # Tentatively add this coordinate to the path.
+        path.append(c)
+        # Set the current coordinate as visited.
+        maze[c.x][c.y] = BLACK
+
+        # Have we reached the exit?
+        if c == e:
+            return True
+
+        # Recursively search for a path from the adjacent cells.
+        adjacent_cells = [Coordinate(x, y) for x, y in [
+            (c.x - 1, c.y), (c.x + 1, c.y), (c.x, c.y - 1), (c.x, c.y + 1)
+        ]]
+        for adj in adjacent_cells:
+            if dfs_search(adj):
+                return True
+
+        # If we couldn't find a path through this cell, remove it.
+        del path[-1]
+        return False
+
+    path: List[Coordinate] = []
+    dfs_search(s)
+    return path
 
 
 def path_element_is_feasible(maze, prev, cur):
