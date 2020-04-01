0c6a556033dc46027eb653b91bb05ab5a86695c6
Andrea Orru <andrea@orru.io>
2020-04-01 21:04:52 +0900

    Sudoku

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -2,10 +2,28 @@ from typing import List
 
 from test_framework import generic_test
 
+from math import sqrt
+
 
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
+    n = len(partial_assignment)
+    sub_n = int(sqrt(n))
+
+    row_sets = [set() for _ in range(n)]
+    col_sets = [set() for _ in range(n)]
+    sub_sets = [[set() for _ in range(sub_n)] for _ in range(sub_n)]
+
+    for y, row in enumerate(partial_assignment):
+        for x, value in enumerate(row):
+            if value and ((value in row_sets[y]) or 
+                          (value in col_sets[x]) or
+                          (value in sub_sets[y // sub_n][x // sub_n])):
+                return False
+            row_sets[y].add(value)
+            col_sets[x].add(value)
+            sub_sets[y // sub_n][x // sub_n].add(value)
+
     return True
 
 
