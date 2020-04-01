38338d17ab891a273e12b16d745ef7898b409254
Andrea Orru <andrea@orru.io>
2020-04-01 16:18:12 +0900

    Transform string

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -2,10 +2,27 @@ from typing import Set
 
 from test_framework import generic_test
 
+from collections import deque, defaultdict
+from string import ascii_lowercase
+
 
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    D.remove(s)
+    queue = deque([(0, s)])
+
+    while queue:
+        depth, curr_str = queue.popleft()
+        if curr_str == t:
+            return depth
+
+        for i in range(len(curr_str)):
+            for c in ascii_lowercase:
+                candidate = curr_str[:i] + c + curr_str[i + 1:]
+                if candidate in D:
+                    D.remove(candidate)
+                    queue.append((depth + 1, candidate))
+
+    return -1
 
 
 if __name__ == '__main__':
