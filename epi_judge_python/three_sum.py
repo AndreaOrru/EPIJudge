c39a8898ce9a48ad1cce0589a2df371fdbef33be
Andrea Orru <andrea@orru.io>
2020-04-01 12:51:55 +0900

    Three sum

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -3,9 +3,24 @@ from typing import List
 from test_framework import generic_test
 
 
+def has_two_sum(A: List[int], t: int) -> bool:
+    left = 0
+    right = len(A) - 1
+
+    while left <= right:
+        if A[left] + A[right] < t:
+            left += 1
+        elif A[left] + A[right] > t:
+            right -= 1
+        else:
+            return True
+
+    return False
+
+
 def has_three_sum(A: List[int], t: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    A.sort()
+    return any(has_two_sum(A, t - a) for a in A)
 
 
 if __name__ == '__main__':
