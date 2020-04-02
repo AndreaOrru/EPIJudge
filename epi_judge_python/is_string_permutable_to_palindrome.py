d2467f1e823b9312ae6dd2bf59fe6b7f51a83758
Andrea Orru <andrea@orru.io>
2020-04-02 21:58:57 +0900

    String permutable to palindrome

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,10 +1,23 @@
 from test_framework import generic_test
 
+from collections import defaultdict, Counter
+
+
+def can_form_palindrome_raw(s: str) -> bool:
+    counts = defaultdict(int)
+    for c in s:
+        counts[c] += 1
+
+    odds = 0
+    for count in counts.values():
+        if count % 2 == 1:
+            odds += 1
+
+    return odds <= 1
+
 
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
-
+    return sum(count % 2 for count in Counter(s).values()) <= 1
 
 if __name__ == '__main__':
     exit(
