50865763deba5e18f60acf3302fb63ad47793506
Andrea Orru <andrea@orru.io>
2020-04-02 14:14:04 +0900

    Circular Queue

--- epi_judge_python/circular_queue.py
+++ epi_judge_python/circular_queue.py
@@ -3,21 +3,39 @@ from test_framework.test_failure import TestFailure
 
 
 class Queue:
+    SCALE_FACTOR = 2
+
     def __init__(self, capacity: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.array = [None] * capacity
+        self.head = self.tail = self.n_elements = 0
 
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._resize()
+
+        self.array[self.tail] = x
+        self.tail = (self.tail + 1) % len(self.array)
+        self.n_elements += 1
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if self.n_elements == 0:
+            raise Exception('No more elements')
+
+        x = self.array[self.head]
+        self.head = (self.head + 1) % len(self.array)
+        self.n_elements -= 1
+        return x
 
     def size(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self.n_elements
+
+    def _resize(self) -> None:
+        if self.n_elements < len(self.array):
+            return
+
+        self.array = self.array[self.head:] + self.array[:self.head]
+        self.array += [None] * ((len(self.array) * self.SCALE_FACTOR) -
+                                 len(self.array))
+        self.head, self.tail = 0, self.n_elements
 
 
 def queue_tester(ops):
