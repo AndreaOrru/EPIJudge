b7aab38b534e42d72a9f9bf01582e47179dd2299
Andrea Orru <andrea@orru.io>
2020-04-02 15:54:24 +0900

    Tree from preorder

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -6,8 +6,29 @@ from test_framework import generic_test
 
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    inorder_indexes = {el: i for i, el in enumerate(inorder)}
+
+    def construct_tree(preorder_start, preorder_end, inorder_start, inorder_end):
+        if preorder_start > preorder_end or inorder_start > inorder_end:
+            return None
+
+        root = preorder[preorder_start]
+        root_inorder_idx = inorder_indexes[root]
+        n_left_elements = root_inorder_idx - inorder_start
+
+        tree = BinaryTreeNode(root)
+        tree.left = construct_tree(preorder_start + 1,
+                                   preorder_start + n_left_elements,
+                                   inorder_start,
+                                   root_inorder_idx - 1)
+        tree.right = construct_tree(preorder_start + n_left_elements + 1,
+                                    preorder_end,
+                                    root_inorder_idx + 1,
+                                    inorder_end)
+        return tree
+
+    return construct_tree(0, len(preorder) - 1,
+                          0, len(inorder) - 1)
 
 
 if __name__ == '__main__':
