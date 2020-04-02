3ebc5e270e31b08fc2f6ce006c8a7ccbe8fd69a6
Andrea Orru <andrea@orru.io>
2020-04-02 19:55:33 +0900

    IP buckets

--- epi_judge_python/absent_value_array.py
+++ epi_judge_python/absent_value_array.py
@@ -3,10 +3,29 @@ from typing import Iterator
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from itertools import tee
+
 
 def find_missing_element(stream: Iterator[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    n_buckets = 1 << 16
+    counters = [0] * n_buckets
+    stream, stream_copy = tee(stream)
+
+    for ip in stream:
+        upper_part_ip = ip >> 16
+        counters[upper_part_ip] += 1
+    
+    bucket_capacity = 1 << 16
+    candidate_bucket = next(i for i, c in enumerate(counters)
+                            if c < bucket_capacity)
+
+    candidates = [0] * bucket_capacity
+    for ip in stream_copy:
+        lower_part_ip = ip & 0xFFFF
+        candidates[lower_part_ip] = 1
+
+    return next(i for i, present in enumerate(candidates)
+                if not present)
 
 
 def find_missing_element_wrapper(stream):
