24e2629097b6d8df55e119b7d223b692479906de
Andrea Orru <andrea@orru.io>
2020-04-02 13:50:29 +0900

    RPN

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,8 +2,31 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    intermediate_results = []
+    operations = {
+        '+': lambda x, y: x  + y,
+        '-': lambda x, y: x  - y,
+        '*': lambda x, y: x  * y,
+        '/': lambda x, y: x // y,
+    }
+
+    i = start = 0
+    while i < len(expression):
+        while i < len(expression) and expression[i] != ',':
+            i += 1
+        
+        op = expression[start: i]
+        if op in operations:
+            y = intermediate_results.pop()
+            x = intermediate_results.pop()
+            intermediate_results.append(operations[op](x, y))
+        else:
+            intermediate_results.append(int(op))
+
+        i += 1
+        start = i
+
+    return intermediate_results[-1]
 
 
 if __name__ == '__main__':
