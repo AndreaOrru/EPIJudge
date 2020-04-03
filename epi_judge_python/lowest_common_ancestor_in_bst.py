85e964b8d021a1f7a522919d34d6c08989c05839
Andrea Orru <andrea@orru.io>
2020-04-03 10:19:34 +0900

    LCA BST

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -10,8 +10,18 @@ from test_framework.test_utils import enable_executor_hook
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    while tree:
+        # Nodes are smaller than the root.
+        if b.data < tree.data:
+            tree = tree.left
+        # Nodes are bigger than the root.
+        elif tree.data < s.data:
+            tree = tree.right
+        # Nodes are equal to the root, or one is smaller and the other bigger.
+        else:
+            break
+
+    return tree
 
 
 @enable_executor_hook
