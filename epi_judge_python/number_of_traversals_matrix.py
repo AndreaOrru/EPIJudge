6887df100024936e6c68c051d88a4795df5627b3
Andrea Orru <andrea@orru.io>
2020-04-03 11:38:01 +0900

    Traversal matrix

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,9 +1,30 @@
 from test_framework import generic_test
+from functools import lru_cache
+
+
+@lru_cache(None)
+def number_of_ways_lru(n: int, m: int) -> int:
+    if n == 0 or m == 0:
+        return 0
+    if n == 1 and m == 1:
+        return 1
+
+    return number_of_ways(n - 1, m) + number_of_ways(n, m - 1)
 
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    n, m = min(n, m), max(n, m)
+    dp = [[0 for _ in range(n + 1)] for _ in range(2)]
+    dp[1][1] = 1
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if i > 1:
+                dp[i % 2][j] = 0
+            dp[i % 2][j] += dp[(i - 1) % 2][j]
+            dp[i % 2][j] += dp[i % 2][j - 1]
+
+    return dp[m % 2][n]
 
 
 if __name__ == '__main__':
