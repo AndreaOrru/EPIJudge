323d77a611b6a80867d8ee95473da73e28d45bb1
Andrea Orru <andrea@orru.io>
2020-04-03 10:37:11 +0900

    Permutations

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -4,8 +4,18 @@ from test_framework import generic_test, test_utils
 
 
 def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def perform_permutations(start: int):
+        if start == len(A) - 1:
+            result.append(A.copy())
+
+        for i in range(start, len(A)):
+            A[start], A[i] = A[i], A[start]
+            perform_permutations(start + 1)
+            A[i], A[start] = A[start], A[i]
+
+    result = []
+    perform_permutations(0)
+    return result
 
 
 if __name__ == '__main__':
