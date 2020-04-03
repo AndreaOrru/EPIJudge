bd0fb68d4149ed56b6e4e31a363092e421618862
Andrea Orru <andrea@orru.io>
2020-04-03 20:35:50 +0900

    Majority search

--- epi_judge_python/majority_element.py
+++ epi_judge_python/majority_element.py
@@ -2,10 +2,22 @@ from typing import Iterator
 
 from test_framework import generic_test
 
+from collections import defaultdict
+
 
 def majority_search(stream: Iterator[str]) -> str:
-    # TODO - you fill in here.
-    return ''
+    candidate_count = 0
+
+    for ch in stream:
+        if candidate_count == 0:
+            candidate = ch
+            candidate_count = 1
+        elif ch == candidate:
+            candidate_count += 1
+        else:
+            candidate_count -= 1
+
+    return candidate
 
 
 def majority_search_wrapper(stream):
