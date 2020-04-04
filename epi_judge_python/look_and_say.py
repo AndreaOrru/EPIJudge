83b6fae670ab992ca151431cc5582e311552c1d4
Andrea Orru <andrea@orru.io>
2020-04-04 12:18:49 +0900

    Look and say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    def next_number(s: str) -> str:
+        result, i = [], 0
+        while i < len(s):
+            count = 1
+            while i + 1 < len(s) and s[i] == s[i + 1]:
+                count, i = count + 1, i + 1
+            result.append(str(count) + s[i])
+            i += 1
+        return ''.join(result)
+
+    s = '1'
+    for _ in range(n - 1):
+        s = next_number(s)
+    return s
 
 
 if __name__ == '__main__':
