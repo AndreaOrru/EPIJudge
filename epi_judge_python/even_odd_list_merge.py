4586b3713eaed4763da6d4dbe89464b540757faa
Andrea Orru <andrea@orru.io>
2020-04-04 12:31:18 +0900

    Even odd merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,8 +5,22 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    even_dummy_head = ListNode(None)
+    odd_dummy_head = ListNode(None)
+
+    tails = [even_dummy_head, odd_dummy_head]
+    turn = 0
+
+    i = 0
+    while L:
+        tails[turn].next = L
+        L = L.next
+        tails[turn] = tails[turn].next
+        turn = not turn
+
+    tails[1].next = None
+    tails[0].next = odd_dummy_head.next
+    return even_dummy_head.next
 
 
 if __name__ == '__main__':
