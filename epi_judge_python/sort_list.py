a682a7cea7447efb59de0c8e77a295d838757fc2
Andrea Orru <andrea@orru.io>
2020-04-05 15:39:40 +0900

    Sort list

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -4,9 +4,39 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
+def merge_sorted_lists(L1: Optional[ListNode],
+                       L2: Optional[ListNode]) -> Optional[ListNode]:
+    dummy_head = tail = ListNode()
+
+    while L1 and L2:
+        if L1.data <= L2.data:
+            tail.next = L1
+            L1 = L1.next
+        else:
+            tail.next = L2
+            L2 = L2.next
+        tail = tail.next
+
+    tail.next = L1 or L2
+
+    return dummy_head.next
+
+
 def stable_sort_list(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    # Base case (0 or 1 elements).
+    if L is None or L.next is None:
+        return L
+
+    # Split list in half.
+    pre_slow, slow, fast = None, L, L
+    while fast and fast.next:
+        pre_slow = slow
+        slow, fast = slow.next, fast.next.next
+    if pre_slow:
+        pre_slow.next = None
+
+    # Merge recursively sorted lists.
+    return merge_sorted_lists(stable_sort_list(L), stable_sort_list(slow))
 
 
 if __name__ == '__main__':
