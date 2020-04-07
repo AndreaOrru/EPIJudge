05c88848942eb69a873a5528ff2575a9d6a762cd
Andrea Orru <andrea@orru.io>
2020-04-07 17:00:12 +0900

    Max trapper water

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -4,8 +4,18 @@ from test_framework import generic_test
 
 
 def get_max_trapped_water(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    left, right = 0, len(heights) - 1
+    max_water = 0
+
+    while left < right:
+        water = (right - left) * min(heights[left], heights[right])
+        max_water = max(max_water, water)
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_water
 
 
 if __name__ == '__main__':
