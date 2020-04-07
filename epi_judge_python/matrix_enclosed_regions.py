c3c9fc215f221890c7e31764a0238a92aa1e4ef7
Andrea Orru <andrea@orru.io>
2020-04-07 17:40:18 +0900

    Enclosed regions

--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -2,10 +2,27 @@ from typing import List
 
 from test_framework import generic_test
 
+from collections import deque
+
 
 def fill_surrounded_regions(board: List[List[str]]) -> None:
-    # TODO - you fill in here.
-    return
+    h, w = len(board), len(board[0])
+    queue = deque()
+
+    for x in range(w):
+        queue.extend([(0, x), (h - 1, x)])
+    for y in range(1, h - 1):
+        queue.extend([(y, 0), (y, w - 1)])
+
+    while queue:
+        y, x = queue.popleft()
+        if 0 <= y < h and 0 <= x < w and board[y][x] == 'W':
+            board[y][x] = 'V'
+            queue.extend([(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)])
+
+    for y in range(h):
+        for x in range(w):
+            board[y][x] = 'B' if board[y][x] != 'V' else 'W'
 
 
 def fill_surrounded_regions_wrapper(board):
