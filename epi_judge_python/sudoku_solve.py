4d4a1e529340c8a64ae1ed458f9517ecbef2a56d
Andrea Orru <andrea@orru.io>
2020-04-07 14:31:22 +0900

    Sudoku

--- epi_judge_python/sudoku_solve.py
+++ epi_judge_python/sudoku_solve.py
@@ -1,16 +1,60 @@
 import copy
 import functools
 import math
-from typing import List
+from typing import List, Tuple, Set
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+from math import sqrt
+
 
 def solve_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
-    return True
+    def find_empty_cell(cell_idx: int) -> Tuple[int, int]:
+        for i in range(cell_idx, n * n):
+            y, x = i // n, i % n
+            if partial_assignment[y][x] == 0:
+                return y, x
+        return None, None
+
+    def possible_values(y: int, x: int) -> Set[int]:
+        result = {v for v in range(1, n + 1)}
+
+        for val in range(1, n + 1)
+        for i in range(n):
+            if partial_assignment[y][i]:
+                result.discard(partial_assignment[y][i])
+            if partial_assignment[i][x]:
+                result.discard(partial_assignment[i][x])
+
+        sub_square_y = (y // sub_n) * sub_n
+        sub_square_x = (x // sub_n) * sub_n
+
+        for i in range(sub_square_y, sub_square_y + sub_n):
+            for j in range(sub_square_x, sub_square_x + sub_n):
+                if partial_assignment[i][j]:
+                    result.discard(partial_assignment[i][j])
+
+        return result
+
+    def partial_solve_sudoku(cell_idx: int) -> bool:
+        y, x = find_empty_cell(cell_idx)
+        if (y, x) == (None, None):
+            return True
+
+        for candidate in possible_values(y, x):
+            partial_assignment[y][x] = candidate
+            if partial_solve_sudoku(y * n + x + 1):
+                return True
+
+        partial_assignment[y][x] = 0
+        return False
+
+    n = len(partial_assignment)
+    sub_n = int(sqrt(n))
+
+    return partial_solve_sudoku(0)
 
 
 def assert_unique_seq(seq):
