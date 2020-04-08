8ed9f67f644636b7c60c3749259e19def6eeda3b
Andrea Orru <andrea@orru.io>
2020-04-08 19:43:10 +0900

    Longest contained interval

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -3,9 +3,42 @@ from typing import List
 from test_framework import generic_test
 
 
+def longest_contained_range_sort(A: List[int]) -> int:
+    A.sort()
+    current = longest = 1
+
+    for i in range(len(A) - 1):
+        if A[i + 1] - A[i] == 1:
+            current += 1
+        elif A[i + 1] - A[i] != 0:
+            current = 1
+        longest = max(longest, current)
+
+    return longest
+
+
 def longest_contained_range(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    elements = set(A)  # O(n) time, space
+    longest = 0
+
+    # O(n)
+    while elements:
+        pivot = elements.pop()
+        current = 1
+
+        candidate = pivot + 1
+        while candidate in elements:
+            elements.remove(candidate)
+            candidate, current = candidate + 1, current + 1
+
+        candidate = pivot - 1
+        while candidate in elements:
+            elements.remove(candidate)
+            candidate, current = candidate - 1, current + 1
+        
+        longest = max(longest, current)
+
+    return longest
 
 
 if __name__ == '__main__':
