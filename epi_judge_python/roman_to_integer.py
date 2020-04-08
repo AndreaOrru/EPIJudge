6c3484d65c0b9aabb5af89160ad09f38d4cb3a07
Andrea Orru <andrea@orru.io>
2020-04-08 13:25:12 +0900

    Roman

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    result = values[s[-1]]
+
+    for i, c in enumerate(s[:-1]):
+        result += -values[c] if values[c] < values[s[i + 1]] else values[c]
+
+    return result
 
 
 if __name__ == '__main__':
