60f8e624834053d50c4eea13bce36f9fe645f5f6
Andrea Orru <andrea@orru.io>
2020-04-08 16:41:40 +0900

    K largest in heap

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -3,9 +3,30 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
+from collections import namedtuple
+from heapq import heappush, heappop
+
+
+Candidate = namedtuple('Candidate', ('value', 'index'))
+
+
 def k_largest_in_binary_heap(A: List[int], k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+
+    candidate_max_heap = [Candidate(-A[0], 0)]
+    for _ in range(k):
+        candidate_idx = candidate_max_heap[0].index
+        result.append(-heappop(candidate_max_heap).value)
+
+        left_child_idx = 2 * candidate_idx + 1
+        if left_child_idx < len(A):
+            heappush(candidate_max_heap, Candidate(-A[left_child_idx], left_child_idx))
+
+        right_child_idx = 2 * candidate_idx + 2
+        if right_child_idx < len(A):
+            heappush(candidate_max_heap, Candidate(-A[right_child_idx], right_child_idx))
+
+    return result
 
 
 if __name__ == '__main__':
