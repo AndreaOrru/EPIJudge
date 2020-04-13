d7d222bce8c0aedfa706393b1450780cee47ea86
Andrea Orru <andrea@orru.io>
2020-04-13 10:42:02 +0900

    Largest rectangle under skyline

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -4,8 +4,17 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    pillar_indexes = []
+    max_rect_area = 0
+
+    for i, h in enumerate(heights + [0]):
+        while pillar_indexes and heights[pillar_indexes[-1]] >= h:
+            height = heights[pillar_indexes.pop()]
+            width = i if not pillar_indexes else i - pillar_indexes[-1] - 1
+            max_rect_area = max(max_rect_area, height * width)
+        pillar_indexes.append(i)
+
+    return max_rect_area
 
 
 if __name__ == '__main__':
