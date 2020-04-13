69578b7e6f4c71d142c0b420ad0f07b5369d73da
Andrea Orru <andrea@orru.io>
2020-04-13 12:13:47 +0900

    GCD

--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -1,10 +1,31 @@
 from test_framework import generic_test
 
 
-def gcd(x: int, y: int) -> int:
+def gcd_fast(x: int, y: int) -> int:
     while y > 0:
         x, y = y, x % y
     return x
+
+
+def gcd(x: int, y: int) -> int:
+    power = 0
+
+    while x != y:
+        x, y = min(x, y), max(x, y)
+
+        if x == 0:
+            return y
+        elif not x & 1 and not y & 1:
+            x, y = x >> 1, y >> 1
+            power += 1
+        elif not x & 1 and y & 1:
+            x, y = x >> 1, y
+        elif x & 1 and not y & 1:
+            x, y = x, y >> 1
+        else:
+            x, y = x, y - x
+
+    return x << power
 
 
 if __name__ == '__main__':
