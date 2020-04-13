1a023745d1d5c0e02d7f42f6244926746278893b
Andrea Orru <andrea@orru.io>
2020-04-13 14:56:41 +0900

    Max of sliding window

--- epi_judge_python/max_of_sliding_window.py
+++ epi_judge_python/max_of_sliding_window.py
@@ -5,16 +5,63 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+from collections import deque
+
+
+class QueueMax:
+    def __init__(self):
+        self.queue = deque()
+        self.maximums = deque()
+
+    def enqueue(self, item):
+        self.queue.append(item)
+        while self.maximums and self.maximums[-1] < item:
+            self.maximums.pop()
+        self.maximums.append(item)
+
+    def dequeue(self):
+        item = self.queue.popleft()
+        if self.maximums[0] == item:
+            self.maximums.popleft()
+        return item
+
+    def head(self):
+        return self.queue[0]
+
+    def max(self):
+        return self.maximums[0]
+
+
 class TrafficElement:
     def __init__(self, time: int, volume: float) -> None:
         self.time = time
         self.volume = volume
 
+    def __lt__(self, other):
+        return self.volume < other.volume
+
+    def __eq__(self, other):
+        return (self.time, self.volume) == (other.time, other.volume)
+
 
 def calculate_traffic_volumes(A: List[TrafficElement],
                               w: int) -> List[TrafficElement]:
-    # TODO - you fill in here.
-    return []
+    A.sort(key=lambda t: t.time)
+    sliding_window = QueueMax()
+    maximum_volumes = []
+
+    for traffic in A:
+        sliding_window.enqueue(traffic)
+        while traffic.time - sliding_window.head().time > w:
+            sliding_window.dequeue()
+        maximum_volumes.append(TrafficElement(traffic.time, sliding_window.max().volume))
+
+    return maximum_volumes
+
+
+
+
+###################################################################
 
 
 @enable_executor_hook
